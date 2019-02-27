6d0ac8d6388ac1186108975337aaa41160ab5a83
Corentin Mathias <corentin.mathias@hec.edu>
2019-02-27 16:05:57 +0100

    ok exercice resto

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
+  root to: "restaurants#index"
+  resources :restaurants, only: [ :index, :new, :create, :show] do
+    resources :reviews, only: [ :new, :create]
+  end
+  #  resources :reviews, only: [:show, :edit, :update, :destroy]
+  # get "restaurants/:id", to: "restaurants#show"
+  # get "reviews/:id", to: "reviews#show"
+  # get "reviews/:id/edit", to: "reviews#edit"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,46 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+puts 'Cleaning database...'
+Restaurant.destroy_all
+
+puts 'Creating restaurants...'
+restaurants_attributes = [
+  {
+    name:         'Dishoom',
+    address:      '7 Boundary St, London E2 7JE',
+    phone_number: '+44 000 00 00 00',
+    category:     'chinese',
+    # rating:        5
+  },
+  {
+    name:         'Pizza East',
+    address:      '56A Shoreditch High St, London E1 6PQ',
+    phone_number: '+44 000 00 00 01',
+    category:     'italian',
+    # rating:        4
+  },
+  {
+    name:         'Boulangerie orange',
+    address:      '18 rue Oberkampf, 75010 Paris',
+    phone_number: '+33 100 00 00 01',
+    category:     'french',
+    # rating:        2
+  },
+  {
+    name:         'chez Guy Savoy',
+    address:      'Quai de la Monnaie, 75010 Paris',
+    phone_number: '+33 100 00 00 11',
+    category:     'french',
+    # rating:        5
+  },
+  {
+    name:         'Baraque à frites',
+    address:      'rue de la Roquette, 75010 Paris',
+    phone_number: '+33 100 00 22 11',
+    category:     'belgian',
+    # rating:        1
+  },
+]
+Restaurant.create!(restaurants_attributes)
+puts 'Finished!'
